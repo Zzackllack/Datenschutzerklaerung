bde4d26b9dbe3fe18cc636f28f635d0c9052c410
Zacklack <contact@zacklack.de>
2025-02-08 04:11:27 +0100

    Add PrivacyPolicy component and integrate HTML content into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Footer from './components/Footer'
-import './App.css'
+import Footer from "./components/Footer";
+import PrivacyPolicyEnhanced from "./components/PrivacyPolicy";
+import privacyContent from "./content/privacyPolicy.html?raw";
+import "./App.css";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-1">
-        {/* Main content will go here */}
+        {/* Using imported HTML content */}
+        <PrivacyPolicyEnhanced htmlContent={privacyContent} />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
